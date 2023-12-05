e18f54c30e0c694908800c2486b0857ae23ea67c
Vestin1023 <vestin1023@gmail.com~>
2023-12-05 12:22:20 +0100

    får jobba på layouten
    går att gå mellan sidorna

--- app/src/main/java/com/example/antons/BordFragment.java
+++ app/src/main/java/com/example/antons/BordFragment.java
@@ -2,63 +2,41 @@ package com.example.antons;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link BordFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+
 public class BordFragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public BordFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment bordFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BordFragment newInstance(String param1, String param2) {
-        BordFragment fragment = new BordFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_bord, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // Använda getView().findViewById inom ett fragment
+        Button addButton = view.findViewById(R.id.addbutton);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Skapa och visa AddOrderFragment
+                AddOrderFragment addOrderFragment = new AddOrderFragment();
+                getParentFragmentManager().beginTransaction()
+                        .replace(R.id.fragmentContainerView, addOrderFragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
     }
 }
