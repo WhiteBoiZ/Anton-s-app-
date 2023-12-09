90382ec5aeace8b8ebea86ed8c6e59ef9a86484f
Alfred Boman <alst2104@student.miun.se>
2023-12-09 12:54:58 +0100

    Fortsatt att man kan lägga till förrätt, varmrätt, dessert samt att man kan gå tillbaka till föregående "fragment".

--- app/src/main/java/com/example/antons/AddOrderFragment.java
+++ app/src/main/java/com/example/antons/AddOrderFragment.java
@@ -1,5 +1,6 @@
 package com.example.antons;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -12,10 +13,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListPopupWindow;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import java.lang.reflect.Array;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -44,8 +47,6 @@ public class AddOrderFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
     }
 
     @Override
@@ -53,15 +54,33 @@ public class AddOrderFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_add_order, container, false);
-
-
+        Button finishOrderButton = view.findViewById(R.id.addToListButton);
+        finishOrderButton.setOnClickListener(this::addToOrder);
 
         String[] starters = {"item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3","item1", "item2", "item3"};
+        String[] mainCourse = {"Lasagne", "Köttbullar"};
+        String[] dessert = {"Glass", "Morotskaka"};
         ListAdapter starterListAdapter = new ListAdapter(requireContext(),R.layout.simple_spinner_item, Arrays.asList(starters));
+        ListAdapter mainCourseAdapter = new ListAdapter(requireContext(),R.layout.simple_spinner_item, Arrays.asList(mainCourse));
+        ListAdapter dessertAdapter = new ListAdapter(requireContext(),R.layout.simple_spinner_item, Arrays.asList(dessert));
         ListPopupWindow starterListPopupWindow = new ListPopupWindow(requireContext());
+        ListPopupWindow maincourseListPopupWindow = new ListPopupWindow(requireContext());
+        ListPopupWindow dessertListPopupWindow = new ListPopupWindow(requireContext());
         starterListPopupWindow.setAnchorView(view.findViewById(R.id.starterSelector));
+        maincourseListPopupWindow.setAnchorView(view.findViewById(R.id.mainCourseSelector));
+        dessertListPopupWindow.setAnchorView(view.findViewById(R.id.dessertSelector));
         starterListPopupWindow.setAdapter(starterListAdapter);
+        maincourseListPopupWindow.setAdapter(mainCourseAdapter);
+        dessertListPopupWindow.setAdapter(dessertAdapter);
+
+
+
         starterListPopupWindow.setHeight(200);
+        maincourseListPopupWindow.setHeight(200);
+        dessertListPopupWindow.setHeight(200);
+
+
+
 
         List<Order> orderList = new ArrayList<Order>(Arrays.asList(new Order("1","Fisk","15:50", "Varmrätt"),
                 new Order("1","Kött","15:50", "Varmrätt")));
@@ -103,12 +122,12 @@ public class AddOrderFragment extends Fragment {
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 String selectedItem = starterListAdapter.getItem(position);
                 System.out.println(selectedItem);
-                orderAdapter.add(new Order(table,selectedItem,"Tid","Typ"));
+                orderAdapter.add(new Order(table,selectedItem,"Tid","Förrätt"));
                 starterListPopupWindow.dismiss();
             }
         });
 
-        // Show the ListPopupWindow when the anchor view is clicked
+        // Show the starters when the anchor view is clicked
         view.findViewById(R.id.starterSelector).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -116,8 +135,58 @@ public class AddOrderFragment extends Fragment {
             }
         });
 
+        maincourseListPopupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = mainCourseAdapter.getItem(i);
+                orderAdapter.add(new Order(table,selectedItem,"Tid","Varmrätt"));
+                maincourseListPopupWindow.dismiss();
+            }
+        });
+
+        view.findViewById(R.id.mainCourseSelector).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                maincourseListPopupWindow.show();
+            }
+        });
+
+        dessertListPopupWindow.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                String selectedItem = dessertAdapter.getItem(i);
+                orderAdapter.add(new Order(table,selectedItem,"Tid","Efterrätt"));
+                dessertListPopupWindow.dismiss();
+            }
+        });
+
+        view.findViewById(R.id.dessertSelector).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dessertListPopupWindow.show();
+            }
+        });
+
+
+
+
+
+
 
 
         return view;
     }
+
+
+    public void addToOrder(View view){
+        //Pass the selected data in the orderList.
+        this.getParentFragmentManager().popBackStack();
+    }
+
+
+
+
+
 }
