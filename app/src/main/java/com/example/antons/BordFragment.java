83f0edb69ec42f13c07056f9b404bce1e3f365cc
Alfred Boman <alst2104@student.miun.se>
2023-12-10 18:27:06 +0100

    Fortsatt på kommunikationen mellan de två fragmenten.

--- app/src/main/java/com/example/antons/BordFragment.java
+++ app/src/main/java/com/example/antons/BordFragment.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,6 +21,13 @@ import java.util.List;
 public class BordFragment extends Fragment implements AddOrderFragment.OnPassOrder{
 
 
+    private RecyclerView starterView;
+    private RecyclerView mainCourseView;
+    private RecyclerView dessertView;
+    private OrderAdapter starterAdapter;
+    private OrderAdapter mainCourseAdapter;
+    private OrderAdapter dessertAdapter;
+
     public static BordFragment newInstance(String table){
         BordFragment bordFragment = new BordFragment();
         Bundle args = new Bundle();
@@ -31,8 +40,19 @@ public class BordFragment extends Fragment implements AddOrderFragment.OnPassOrd
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bord, container, false);
+        View view = inflater.inflate(R.layout.fragment_bord, container, false);
+        System.out.println("View created");
+        starterView = view.findViewById(R.id.starterOrders);
+        starterView.setLayoutManager(new LinearLayoutManager(this.requireContext()));
+        starterView.setAdapter(starterAdapter);
+        mainCourseView = view.findViewById(R.id.mainCourseOrders);
+        mainCourseView.setLayoutManager(new LinearLayoutManager(this.requireContext()));
+        mainCourseView.setAdapter(mainCourseAdapter);
+        dessertView = view.findViewById(R.id.dessertOrders);
+        dessertView.setLayoutManager(new LinearLayoutManager(this.requireContext()));
+        dessertView.setAdapter(dessertAdapter);
+        System.out.println("View oncreate");
+        return view;
     }
 
     @Override
@@ -52,18 +72,8 @@ public class BordFragment extends Fragment implements AddOrderFragment.OnPassOrd
         addMainCourse.setOnClickListener(this::onClick);
         addDessert.setOnClickListener(this::onClick);
 
-        /*
-        addButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Skapa och visa AddOrderFragment
-                AddOrderFragment addOrderFragment = new AddOrderFragment();
-                getParentFragmentManager().beginTransaction()
-                        .replace(R.id.fragmentContainerView, addOrderFragment)
-                        .addToBackStack(null)
-                        .commit();
-            }
-        });*/
+
+
     }
 
 
@@ -80,6 +90,7 @@ public class BordFragment extends Fragment implements AddOrderFragment.OnPassOrd
         if(view.getId() == R.id.addMainCourse){
             System.out.println("Varmrätt");
             AddOrderFragment addOrderFragment = new AddOrderFragment("Varmrätt", this.getArguments().getString("tableID"));
+            addOrderFragment.setOnPassOrder(this);
             getParentFragmentManager().beginTransaction()
                     .replace(R.id.fragmentContainerView, addOrderFragment)
                     .addToBackStack(null)
@@ -88,6 +99,7 @@ public class BordFragment extends Fragment implements AddOrderFragment.OnPassOrd
         if(view.getId() == R.id.addDessert){
             System.out.println("Dessert");
             AddOrderFragment addOrderFragment = new AddOrderFragment("Dessert", this.getArguments().getString("tableID"));
+            addOrderFragment.setOnPassOrder(this);
             getParentFragmentManager().beginTransaction()
                     .replace(R.id.fragmentContainerView, addOrderFragment)
                     .addToBackStack(null)
@@ -97,7 +109,23 @@ public class BordFragment extends Fragment implements AddOrderFragment.OnPassOrd
 
     @Override
     public void onDataPassed(List<Order> list) {
-        System.out.println("In the fragment now...");
-        System.out.println("In fragment" + list.get(0).getOrder() + list.get(2).getOrder());
+        System.out.println(list.get(0).getType());
+        if(!list.isEmpty()){
+            switch(list.get(0).getType()){
+                case "Förrätt":
+                    System.out.println("Sets list");
+                    starterAdapter = new OrderAdapter(list);
+                    break;
+                case "Varmrätt":
+                    System.out.println("Varmrätt-lista");
+                    mainCourseAdapter = new OrderAdapter(list);
+                    break;
+                case "Dessert":
+                    System.out.println("Dessert-lista");
+                    dessertAdapter = new OrderAdapter(list);
+                    break;
+            }
+        }
+        System.out.println("In fragment" + list.get(0).getOrder() + list.get(1).getOrder());
     }
 }
